656501af742d653016e0acd64ed32527a5680725
Shun Liu <u7797828@anu.edu.au>
2025-01-29 23:05:25 +1100

    finish

--- app/legal/privacy.tsx
+++ app/legal/privacy.tsx
@@ -1,16 +1,17 @@
-import { AppBar } from '@/components/layout/app-bar';
 import { View } from '@/components/ui/view';
 import { WebView } from 'react-native-webview';
 
 export default function PrivacyScreen() {
   return (
     <View className="flex-1 bg-background-0">
-      <AppBar title="Privacy Policy" />
       <WebView
         className="flex-1"
         source={{
           uri: 'https://shunl12324.github.io/comfy-portal/privacy.html',
         }}
+        cacheEnabled={false}
+        cacheMode="LOAD_NO_CACHE"
+        incognito={true}
       />
     </View>
   );
--- app/legal/terms.tsx
+++ app/legal/terms.tsx
@@ -1,16 +1,17 @@
-import { AppBar } from '@/components/layout/app-bar';
 import { View } from '@/components/ui/view';
 import { WebView } from 'react-native-webview';
 
 export default function TermsScreen() {
   return (
     <View className="flex-1 bg-background-0">
-      <AppBar title="Terms of Service" />
       <WebView
         className="flex-1"
         source={{
           uri: 'https://shunl12324.github.io/comfy-portal/terms.html',
         }}
+        cacheEnabled={false}
+        cacheMode="LOAD_NO_CACHE"
+        incognito={true}
       />
     </View>
   );
